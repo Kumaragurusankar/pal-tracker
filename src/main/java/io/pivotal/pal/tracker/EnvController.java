2437e58b04a3865aca93d764eed00cd394e0f8d4
kumaragurusankar <kumaraguru.marees@gmail.com>
2019-07-29 16:59:36 -0400

    Added the main class name

--- src/main/java/io/pivotal/pal/tracker/EnvController.java
+++ src/main/java/io/pivotal/pal/tracker/EnvController.java
@@ -1,4 +1,41 @@
 package io.pivotal.pal.tracker;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
 public class EnvController {
+
+    String port;
+
+    String memoryLimit;
+
+    String cfInstanceIndex;
+
+    String cfInstanceAddr;
+
+
+
+    public EnvController(String port, String memoryLimit, String cfInstanceIndex, String cfInstanceAddr) {
+        this.port = port;
+        this.memoryLimit = memoryLimit;
+        this.cfInstanceIndex = cfInstanceIndex;
+        this.cfInstanceAddr = cfInstanceAddr;
+    }
+
+    @GetMapping("/env")
+    public Map<String, String> getEnv() {
+
+        Map<String, String> envMap = new HashMap<String, String>();
+        envMap.put("PORT", port);
+        envMap.put("MEMORY_LIMIT", memoryLimit);
+        envMap.put("CF_INSTANCE_INDEX", cfInstanceIndex);
+        envMap.put("CF_INSTANCE_ADDR", cfInstanceAddr);
+        return envMap;
+    }
+
 }
--- src/main/java/io/pivotal/pal/tracker/PalTrackerApplication.java
+++ src/main/java/io/pivotal/pal/tracker/PalTrackerApplication.java
@@ -1,4 +1,41 @@
 package io.pivotal.pal.tracker;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
 public class PalTrackerApplication {
+
+
+    @Value("${PORT:NOT SET}")
+    String port;
+
+    @Value("${MEMORY_LIMIT:NOT SET}")
+    String memoryLimit;
+
+    @Value("${CF_INSTANCE_INDEX:NOT SET}")
+    String cfInstanceIndex;
+
+    @Value("${CF_INSTANCE_ADDR:NOT SET}")
+    String cfInstanceAddr;
+
+    @Value("${WELCOME_MESSAGE}")
+    String message;
+
+    public static void main(String[] args) {
+        SpringApplication.run(PalTrackerApplication.class, args);
+    }
+
+    @Bean
+    public EnvController envController(){
+        return new EnvController(port, memoryLimit, cfInstanceIndex , cfInstanceAddr);
+    }
+
+    @Bean
+    public WelcomeController welcomeController(){
+        return new WelcomeController(message);
+    }
+
 }
